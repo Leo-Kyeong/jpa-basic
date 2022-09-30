cfb376476af7c12245a050d9c507ec00885ce194
KyeongWoo Ryu <drd9811@naver.com>
2022-09-30 16:22:45 +0900

    PersistenceContext

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -13,16 +13,34 @@ public class JpaMain {
 		tx.begin();
 
 		try {
-			// Member findMember = em.find(Member.class, 1L);
-			List<Member> result = em.createQuery("select m from Member as m", Member.class)
-					.setFirstResult(1)
-					.setMaxResults(10)
-					.getResultList();
+			// INSERT
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloA");
+            em.persist(member);
 
-			for (Member member : result) {
-				System.out.println("member.name = " + member.getName());
-			}
-			tx.commit();
+            // SELECT(단건 조회)
+            Member findMember = em.find(Member.class, 1L);
+
+            // SELECT(다중건 조회)
+            // setFirstResult(int startPosition) : 조회 시작 위치
+            // setMaxResults(int maxResult) : 조회할 데이터 수
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            for (Member memberFind : result) {
+                System.out.println("member.getName = "+ memberFind.getName());
+            }
+
+            // DELETE
+            em.remove(findMember);
+
+            // UPDATE(dirty checking)
+            findMember.setName("HelloJPA");
+
+            tx.commit(); // Transaction 저장
 		} catch (Exception e) {
 			tx.rollback();
 		} finally {
---
+++ src/main/java/hellojpa/PersistenceContext.java
@@ -0,0 +1,41 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class PersistenceContext {
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+		EntityManager em = emf.createEntityManager();
+
+		EntityTransaction tx = em.getTransaction();
+		tx.begin(); // Transaction 시작
+
+		try {
+			// 비영속
+			Member member = new Member();
+			member.setId(100L);
+			member.setName("HelloB");
+
+			// 영속
+			em.persist(member);
+
+			// 준영속 (영속성 컨텍스트에서 분리)
+			em.detach(member);
+
+			// 객체를 삭제한 상태
+			em.remove(member);
+
+            tx.commit(); // Transaction 저장
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			em.close();
+		}
+		emf.close();
+	}
+}
