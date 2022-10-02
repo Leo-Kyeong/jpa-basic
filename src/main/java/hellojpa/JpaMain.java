455e24f3c88b2cacb78d7b17384bcc3cc6cff6c2
kyeongwoo <drd9811@naver.com>
2022-10-02 18:17:01 +0900

    JpaMain

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import jpabook.jpashop.domain.Member;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -13,32 +15,32 @@ public class JpaMain {
 		tx.begin(); // Transaction 시작
 
 		try {
-			// INSERT
-            Member member = new Member();
-            member.setId(2L);
-            member.setUsername("HelloA");
-            em.persist(member);
-
-            // SELECT(단건 조회)
-            Member findMember = em.find(Member.class, 1L);
-
-            // SELECT(다중건 조회)
-            // setFirstResult(int startPosition) : 조회 시작 위치
-            // setMaxResults(int maxResult) : 조회할 데이터 수
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
-
-            for (Member memberFind : result) {
-                System.out.println("member.getName = "+ memberFind.getUsername());
-            }
-
-            // DELETE
-            em.remove(findMember);
-
-            // UPDATE(dirty checking)
-            findMember.setUsername("HelloJPA");
+//			// INSERT
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setUsername("HelloA");
+//            em.persist(member);
+//
+//            // SELECT(단건 조회)
+//            Member findMember = em.find(Member.class, 1L);
+//
+//            // SELECT(다중건 조회)
+//            // setFirstResult(int startPosition) : 조회 시작 위치
+//            // setMaxResults(int maxResult) : 조회할 데이터 수
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(5)
+//                    .setMaxResults(8)
+//                    .getResultList();
+//
+//            for (Member memberFind : result) {
+//                System.out.println("member.getName = "+ memberFind.getUsername());
+//            }
+//
+//            // DELETE
+//            em.remove(findMember);
+//
+//            // UPDATE(dirty checking)
+//            findMember.setUsername("HelloJPA");
 
             tx.commit(); // Transaction 저장
 		} catch (Exception e) {
