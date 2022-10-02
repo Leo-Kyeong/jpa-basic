91d7ae55a8d4685d3295f39579914df68ae5b397
kyeongwoo <drd9811@naver.com>
2022-10-02 18:17:35 +0900

    JpaShop

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Member {
+
+	@Id @GeneratedValue
+	@Column(name = "MEMBER_ID")
+	private Long id;
+
+	private String name;
+
+	private String city;
+	private String street;
+	private String zipcode;
+}
