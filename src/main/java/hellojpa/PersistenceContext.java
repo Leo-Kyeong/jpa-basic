fe56899f87685c4e9e3e0002a9a3c0d6f404f694
kyeongwoo <drd9811@naver.com>
2022-10-02 18:17:44 +0900

    PersistenceContext

--- src/main/java/hellojpa/PersistenceContext.java
+++ src/main/java/hellojpa/PersistenceContext.java
@@ -1,10 +1,11 @@
 package hellojpa;
 
+import jpabook.jpashop.domain.Member;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class PersistenceContext {
 	public static void main(String[] args) {
@@ -16,19 +17,19 @@ public class PersistenceContext {
 		tx.begin(); // Transaction 시작
 
 		try {
-			// 비영속
-			Member member = new Member();
-			member.setId(100L);
-			member.setUsername("HelloB");
-
-			// 영속
-			em.persist(member);
-
-			// 준영속 (영속성 컨텍스트에서 분리)
-			em.detach(member);
-
-			// 객체를 삭제한 상태
-			em.remove(member);
+//			// 비영속
+//			Member member = new Member();
+//			member.setId(100L);
+//			member.setUsername("HelloB");
+//
+//			// 영속
+//			em.persist(member);
+//
+//			// 준영속 (영속성 컨텍스트에서 분리)
+//			em.detach(member);
+//
+//			// 객체를 삭제한 상태
+//			em.remove(member);
 
             tx.commit(); // Transaction 저장
 		} catch (Exception e) {
