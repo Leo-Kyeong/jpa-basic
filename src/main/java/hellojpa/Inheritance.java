22cc6519f2a5d945fa78900edfc215db6ea321f1
Leo <drd9811@naver.com>
2022-11-20 20:13:36 +0900

    상속 관계 매핑 - 슈퍼/서브타입 관계

---
+++ src/main/java/hellojpa/Inheritance.java
@@ -0,0 +1,40 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Inheritance {
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+		EntityManager em = emf.createEntityManager();
+
+		EntityTransaction tx = em.getTransaction();
+		tx.begin(); // Transaction 시작
+
+		try {
+			Movie movie = new Movie();
+			movie.setDirector("감독A");
+			movie.setActor("배우A");
+			movie.setName("바람과 함께 사라지다");
+			movie.setPrice(10000);
+
+			em.persist(movie);
+
+			em.flush();
+			em.clear();
+
+			Movie findMovie = em.find(Movie.class, movie.getId());
+			System.out.println("findMovie = " + findMovie);
+
+			tx.commit(); // Transaction 저장
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			em.close();
+		}
+		emf.close();
+	}
+}
---
+++ src/main/java/hellojpa/Item.java
@@ -0,0 +1,23 @@
+package hellojpa;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+
+@Entity
+@Getter
+@Setter
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn
+public abstract class Item {
+
+	@Id
+	@GeneratedValue
+	Long id;
+
+	String name;
+	int price;
+}
